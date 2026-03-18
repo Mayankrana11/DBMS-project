dec130880d1d502c5e5c0b257b1b9edb31bb1e8b
Mayank Rana <mayank24341@iiitd.ac.in>
2026-03-18 11:43:59 +0530

    ride route

---
+++ backend/routes/rideRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const rideController = require("../controllers/rideController");
+
+router.post("/book", rideController.bookRide);
+
+module.exports = router;
