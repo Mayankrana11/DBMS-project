c8f01709cc5bcbb327ded67a462b7c91e231828c
Mayank Rana <mayank24341@iiitd.ac.in>
2026-03-19 10:57:44 +0530

    updated locks

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,27 +1,26 @@
-const API = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000/api";
 
 export const login = async (data) => {
-  const res = await fetch(`${API}/auth/login`, {
+  const res = await fetch(`${BASE_URL}/auth/login`, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(data),
   });
+
   return res.json();
 };
 
 export const bookRide = async (token, data) => {
-  const res = await fetch(`${API}/rides/book`, {
+  const res = await fetch(`${BASE_URL}/rides/book`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: token,
+      "Authorization": token,
     },
     body: JSON.stringify(data),
   });
-  return res.json();
-};
 
-export const getDrivers = async () => {
-  const res = await fetch(`${API}/drivers`);
   return res.json();
 };
