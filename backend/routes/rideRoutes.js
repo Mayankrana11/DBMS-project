c07fe114da303c03ee389df346249aab7f47ee0b
Mayank Rana <mayank24341@iiitd.ac.in>
2026-03-22 16:11:17 +0530

    ride routes update for accept complete and cancel

--- backend/routes/rideRoutes.js
+++ backend/routes/rideRoutes.js
@@ -3,5 +3,14 @@ const router = express.Router();
 const rideController = require("../controllers/rideController");
 const { verifyToken } = require("../middleware/authMiddleware");
 
+
+// USER routes
 router.post("/book", verifyToken, rideController.bookRide);
+
+// DRIVER routes
+router.get("/requested", verifyToken, rideController.getRequestedRides);
+router.post("/accept", verifyToken, rideController.acceptRide);
+router.post("/complete", verifyToken, rideController.completeRide);
+router.post("/cancel", verifyToken, rideController.cancelRide);
+
 module.exports = router;
