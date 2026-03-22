38e0f61c932f8b87b6142f67119b43bad84c8441
Mayank Rana <mayank24341@iiitd.ac.in>
2026-03-22 16:12:36 +0530

    triggers logic update

--- database/triggers.sql
+++ database/triggers.sql
@@ -9,9 +9,7 @@ BEGIN
     IF NEW.ride_status = 'completed' THEN
         UPDATE DRIVER
         SET availability_status = 'available'
-        WHERE driver_id IN (
-            SELECT driver_id FROM RATING WHERE ride_id = NEW.ride_id
-        );
+        WHERE driver_id = NEW.driver_id;
     END IF;
 END$$
 
@@ -21,14 +19,14 @@ DELIMITER ;
 DELIMITER $$
 
 CREATE TRIGGER after_payment_success
-AFTER INSERT ON PAYMENT
+AFTER UPDATE ON PAYMENT
 FOR EACH ROW
 BEGIN
     IF NEW.payment_status = 'success' THEN
         UPDATE ACCOUNT
         SET balance = balance - NEW.amount
-        WHERE user_id IN (
-            SELECT user_id FROM RATING WHERE ride_id = NEW.ride_id
+        WHERE user_id = (
+            SELECT user_id FROM RIDE WHERE ride_id = NEW.ride_id
         );
     END IF;
 END$$
